cbc0b5178d02822c34525371786f2bcf8b1278a2
seungmun <tjdans174@gmail.com>
2018-04-20 13:21:09 +0900

    update : Change EventHandler  callEventByAsynch method and callEvent method paparameter into CallBackEvent

--- src/Control/EventHandler.java
+++ src/Control/EventHandler.java
@@ -26,7 +26,7 @@ public class EventHandler {
 		}
 	}
 
-	private synchronized void callEventByAsynch(final Class<?> caller, final Object event) {
+	private synchronized void callEventByAsynch(final Class<?> caller, final CallBackEvent event) {
 
 		for (final EventListener listener : listeners) {
 			if (listener.getClass().getName().equals(caller.getName())) {
@@ -36,36 +36,9 @@ public class EventHandler {
 
 	}
 
-	public synchronized void callEvent(final Class<?> caller, Object msg1) {
+	public synchronized void callEvent(final Class<?> caller, CallBackEvent msg1) {
 		callEventByAsynch(caller, msg1);
 	}
 
-	public synchronized void quickCallEvent(final Class<?> caller, Object msg1) {
-		quickCallEvent(caller, msg1, null);
-	}
-
-	public synchronized void quickCallEvent(final Class<?> caller, Object msg1, Object msg2) {
-		quickCallEvent(caller, msg1, msg2, null);
-	}
-
-	public synchronized void quickCallEvent(final Class<?> caller, Object msg1, Object msg2, Object msg3) {
-		quickCallEvent(caller, msg1, msg2, msg3, null);
-	}
-
-	public synchronized void quickCallEvent(final Class<?> caller, Object msg1, Object msg2, Object msg3, Object msg4) {
-		quickCallEvent(caller, msg1, msg2, msg3, msg4, null);
-	}
-
-	public synchronized void quickCallEvent(final Class<?> caller, Object msg1, Object msg2, Object msg3, Object msg4,
-			Object msg5) {
-		TotalJsonObject msg=new TotalJsonObject();
-		Object[] msgs = { msg1, msg2, msg3, msg4, msg5 };
-		for (int i = 0; i < msgs.length; i++) {
-			if (msgs[i] == null)
-				break;
-			msg.addProperty(msgs[i].getClass().getName(), msgs[i].toString());
-		}
-		callEventByAsynch(caller, msg.toString());
-	}
 
 }
