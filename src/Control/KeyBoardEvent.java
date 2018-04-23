190e947281f920fd890e013d029e1415cf5c1cc4
seungmun <tjdans174@gmail.com>
2018-04-23 16:51:59 +0900

    update : Change KeyBoardEvent Class

--- src/Control/KeyBoardEvent.java
+++ src/Control/KeyBoardEvent.java
@@ -10,41 +10,35 @@ import Serversynchronization.TotalJsonObject;
 
 public class KeyBoardEvent extends KeyAdapter implements KeyListener {
 	ImagePrint mainprint;
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		TotalJsonObject moveMessage = new TotalJsonObject();
 		int key = e.getKeyCode();
+		CallBackEvent event = null;
 		switch (key) {
 		case KeyEvent.VK_RIGHT:
-			moveMessage.addProperty("method", "TetrinoBlockMove");
-			moveMessage.addProperty("MoveType", MoveType.RIGHT.toString());
-			MVC_Connect.ControlToModel.callEvent(TetrisManager.class, moveMessage.toString());
+			event = (object) -> ((TetrisManager) object).TetrinoBlockMove(MoveType.RIGHT);
 			break;
 		case KeyEvent.VK_LEFT:
-			moveMessage.addProperty("method", "TetrinoBlockMove");
-			moveMessage.addProperty("MoveType", MoveType.LEFT.toString());
-			MVC_Connect.ControlToModel.callEvent(TetrisManager.class, moveMessage.toString());
+			event = (object) -> ((TetrisManager) object).TetrinoBlockMove(MoveType.LEFT);
 			break;
 		case KeyEvent.VK_UP:
-			moveMessage.addProperty("method", "TetrinoBlockMove");
-			moveMessage.addProperty("MoveType", MoveType.TURN.toString());
-			MVC_Connect.ControlToModel.callEvent(TetrisManager.class, moveMessage.toString());
+			event = (object) -> ((TetrisManager) object).TetrinoBlockMove(MoveType.TURN);
 			break;
 		case KeyEvent.VK_DOWN:
-			moveMessage.addProperty("method", "TetrinoBlockMove");
-			moveMessage.addProperty("MoveType", MoveType.DOWN.toString());
-			MVC_Connect.ControlToModel.callEvent(TetrisManager.class, moveMessage.toString());
+			event = (object) -> ((TetrisManager) object).TetrinoBlockMove(MoveType.DOWN);
 			break;
 		case KeyEvent.VK_SPACE:
-			moveMessage.addProperty("method", "TetrinoBlockDropMove");
-			moveMessage.addProperty("MoveType", MoveType.DROP.toString());
-			MVC_Connect.ControlToModel.callEvent(TetrisManager.class, moveMessage.toString());
+			event = (object) -> ((TetrisManager) object).TetrinoBlockMove(MoveType.DROP);
 			break;
 		case KeyEvent.VK_Z:
-			moveMessage.addProperty("method", "saveBlock");
-			MVC_Connect.ControlToModel.callEvent(TetrisManager.class, moveMessage.toString());
+			event = (object) -> ((TetrisManager) object).saveBlock();
 			break;
+		default:
+			return;
 		}
+		MVC_Connect.ControlToModel.callEvent(TetrisManager.class, event);
 	}
 
 }
