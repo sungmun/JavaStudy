4bacf0536f408ff6fdeefd092c306255b9426c1e
seungmun <tjdans174@gmail.com>
2018-04-28 14:25:40 +0900

    update : change FrameControl Class

--- src/Control/FrameControl.java
+++ src/Control/FrameControl.java
@@ -5,8 +5,7 @@ import javax.swing.JOptionPane;
 
 public class FrameControl implements EventListener {
 	public FrameControl() {
-		MVC_Connect.ModelToControl.addListener(this);
-		MVC_Connect.ViewToControl.addListener(this);
+		MVC_Connect.Controller.addListener(this);
 	}
 
 	public void FrameChange(Class<?> fropen, Class<?> frclose) {
@@ -15,14 +14,13 @@ public class FrameControl implements EventListener {
 	}
 
 	public void FrameClose(Class<?> frclose) {
-		MVC_Connect.ControlToView.callEvent(frclose, (fr) -> ((JFrame) fr).dispose());
+		MVC_Connect.View.callEvent(frclose, (fr) -> ((JFrame) fr).dispose());
 	}
 
 	public void FrameOpen(Class<?> fropen) {
-
 		try {
 			fropen.newInstance();
-			MVC_Connect.ControlToView.callEvent(fropen, (fr) -> ((JFrame) fr).setVisible(true));
+			MVC_Connect.View.callEvent(fropen, (fr) -> ((JFrame) fr).setVisible(true));
 		} catch (InstantiationException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
@@ -35,29 +33,6 @@ public class FrameControl implements EventListener {
 	public void showMessageDialog(String title, String content) {
 		JOptionPane.showMessageDialog(null, content, title, JOptionPane.PLAIN_MESSAGE);
 	}
-	//
-	// @Override
-	// public void onEvent(Object event) {
-	// System.out.println("FrameControl.onEvent()");
-	// TotalJsonObject object = new TotalJsonObject((String) event);
-	// methodCatch(object);
-	// }
-	//
-	// public void methodCatch(Object event) {
-	// TotalJsonObject obj = (TotalJsonObject) event;
-	// switch (obj.get("method").toString()) {
-	// case "FrameChange":
-	// FrameChange(obj);
-	// break;
-	// case "FrameOpen":
-	// FrameOpen(obj);
-	// break;
-	// case "FrameClose":
-	// FrameClose(obj);
-	// break;
-	// }
-	//
-	// }
 
 	@Override
 	public void onEvent(CallBackEvent event) {
